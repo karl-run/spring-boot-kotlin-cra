a19d34838329995bac3db7daab016b02d9d5bd7b
Karl J. Overå <karloveraa@gmail.com>
2018-03-25 13:23:32 +0200

    Add example controller

--- server/src/main/kotlin/run/karl/starter/example/ExampleController.kt
+++ server/src/main/kotlin/run/karl/starter/example/ExampleController.kt
@@ -1,2 +1,11 @@
 package run.karl.starter.example
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ExampleController {
+
+    @GetMapping("hello")
+    fun example() = "Hello World!"
+}
