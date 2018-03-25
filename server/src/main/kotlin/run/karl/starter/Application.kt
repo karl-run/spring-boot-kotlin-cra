19f755f6f970cb20f12e6a296cda06a18931ed21
Karl J. Overå <karloveraa@gmail.com>
2018-03-25 13:27:08 +0200

    Rename StarterApplication.kt -> Application.kt

--- server/src/main/kotlin/run/karl/starter/Application.kt
+++ server/src/main/kotlin/run/karl/starter/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class StarterApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<StarterApplication>(*args)
+    runApplication<Application>(*args)
 }
--- server/src/main/kotlin/run/karl/starter/example/ExampleController.kt
+++ server/src/main/kotlin/run/karl/starter/example/ExampleController.kt
@@ -3,7 +3,7 @@ package run.karl.starter.example
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@RestController()
 class ExampleController {
 
     @GetMapping("hello")
