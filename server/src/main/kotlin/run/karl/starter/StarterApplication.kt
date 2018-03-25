65cd5dd19cdb2c98540d776769902aa3f1d8b438
Karl J. Overå <karloveraa@gmail.com>
2018-03-25 12:59:13 +0200

    Initial project structure

---
+++ server/src/main/kotlin/run/karl/starter/StarterApplication.kt
@@ -0,0 +1,11 @@
+package run.karl.starter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StarterApplication
+
+fun main(args: Array<String>) {
+    runApplication<StarterApplication>(*args)
+}
